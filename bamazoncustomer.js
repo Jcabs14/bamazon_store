49cbe5a05aa561d54ed9dc4c26a0a6efb262b732
Jared Cabanilla <Jcabanilla@gmail.com>
2019-08-13 15:59:43 -0700

    added new js file

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -1,2 +1,18 @@
 var mysql = require('mysql');
 var inqurier = require('inqurier');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "MaryAnna1!",
+    database: "bamazon"
+  });
+
+  function 
